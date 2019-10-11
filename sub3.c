7ea5b6f3d011c61c2b9b994d24485aee942ae1cc
rofl0r <rofl0r@users.noreply.github.com>
2019-10-11 20:06:38 +0100

    sub3.c: fix gcc -Wall warnings

--- sub3.c
+++ sub3.c
@@ -145,12 +145,13 @@ remch(wchar_t c)
 	 * Make sure no EUC chars are used in reg. exp.
 	 */
 	if (!handleeuc) {
-		if (!isascii(c))
+		if (!isascii(c)) {
 		    if (iswprint(c))
 			warning(
 "Non-ASCII character '%lc' in pattern; use -w or -e lex option.", c);
 		    else warning(
 "Non-ASCII character of value %#x in pattern; use -w or -e lex option.", c);
+		}
 		/* In any case, we don't need to construct ncgidtbl[]. */
 		return;
 	}
@@ -303,7 +304,7 @@ repbycgid(void)
 				symbol[j] = FALSE;
 
 			s = (CHR *) left[i];
-			while (cc = *s++) {
+			while ((cc = *s++)) {
 				if (cc == RANGE) {
 					int	low, high, i;
 					/*
