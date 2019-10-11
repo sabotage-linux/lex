e0cd255d7d2bcd4c7e21084d43a6a003afc73afe
rofl0r <rofl0r@users.noreply.github.com>
2019-10-11 19:42:49 +0100

    once.h: import latest opensolaris changes

--- once.h
+++ once.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,13 +19,15 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.
- * All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
 /*	Copyright (c) 1988 AT&T	*/
 /*	All Rights Reserved	*/
+
+#ifndef	_ONCE_H
+#define	_ONCE_H
 
 /*	from OpenSolaris "once.h	6.9	05/06/08 SMI"	*/
 
@@ -164,3 +165,5 @@ Boolean handleeuc = FALSE;
 Boolean widecio = FALSE;
 
 int	isArray = 1;	/* XCU4: for %array %pointer */
+
+#endif /* _ONCE_H */
